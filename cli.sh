0e5b7b2447ebf800801427951204ed205e1253cf
Isak ✏ ⇝ <isak.burstrom@gmail.com>
2023-02-24 16:47:57 +0100

    Update cli.sh

--- cli.sh
+++ cli.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-full_path="$(realpath "$0")"
-dir_path="$(dirname $full_path)"
 script_path="./lib/cli.js"
 
 node_path="$(which node)" # <== Makes it work on github codespaces 😅
