98298ae23009188a1f94a8a519b76e8252ad5305
Isak ✏ ⇝ <isak.burstrom@gmail.com>
2023-02-24 16:47:01 +0100

    Remove dir path

--- cli.sh
+++ cli.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 full_path="$(realpath "$0")"
 dir_path="$(dirname $full_path)"
-script_path="$dir_path./lib/cli.js"
+script_path="./lib/cli.js"
 
 node_path="$(which node)" # <== Makes it work on github codespaces 😅
 
